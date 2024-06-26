eaea1f2284af3ef4b6ac9c815f84c2cbbb4ec463
ALUNO <ALUNO@LAB04-PC32>
2024-06-26 19:51:18 -0300

    Commit project Fliperama_V0.3

--- src/fliperama/FliperamaTeste.java
+++ src/fliperama/FliperamaTeste.java
@@ -1,31 +1,30 @@
 
 package fliperama;
 
+
 public class FliperamaTeste {
 
+    
    
     public static void main(String[] args) {
-
-        
-        
+   
         Cartao cartao01 = new Cartao(1234);
         Cartao cartao02 = new Cartao(2345);
+       
+        Terminal.recarga(cartao01, 300);
+        Terminal.recarga(cartao02, 200);
         
         Terminal.display(cartao01);
-        Terminal.recarga(cartao01, 100);
-        
-       
-       Terminal.display(cartao02);
-       Terminal.recarga(cartao02, 200);
-       
-       Terminal.transferir(cartao01, cartao02, 20);
-        
-       
-       Jogo.jogo(cartao02);
-       Jogo.jogo(cartao02);
-       
-       Terminal.Premios(cartao02);
-        
-    }
+        Terminal.display(cartao02);
     
+        Terminal.transferir(cartao01, cartao02, 100);
+        
+        Jogo.jogo(cartao01);
+        Jogo.jogo(cartao01);
+        
+        Jogo.jogo(cartao02);
+        Jogo.jogo(cartao02);
+        
+        Terminal.Premios(cartao02);
+    }
 }
